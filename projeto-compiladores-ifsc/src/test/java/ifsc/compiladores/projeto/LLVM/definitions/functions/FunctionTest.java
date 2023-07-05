39d310ab55bfd3d4daad6dad4b9a3479d2fe9c78
Alessandra <alessandra@atplus.com.br>
2023-07-05 14:36:37 -0300

    Criando testes para Function

---
+++ projeto-compiladores-ifsc/src/test/java/ifsc/compiladores/projeto/LLVM/definitions/functions/FunctionTest.java
@@ -0,0 +1,86 @@
+package ifsc.compiladores.projeto.LLVM.definitions.functions;
+
+import ifsc.compiladores.projeto.LLVM.definitions.Variable;
+import ifsc.compiladores.projeto.LLVM.definitions.types.BaseType;
+import ifsc.compiladores.projeto.LLVM.definitions.types.Type;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunctionTest {
+
+    @Test
+    void shouldEmit_function_withValueReturnType_withoutParameters_usingGetText() {
+        // Arrange
+        BaseType baseType = BaseType.INT;
+        Type typeWithoutDimensions = new Type(baseType);
+        Function function = new Function(typeWithoutDimensions, "myFunction");
+
+        // Act
+        String emitValue = function.getText();
+
+        // Assert
+        assertEquals("define i32 @myFunction() {\n}", emitValue);
+    }
+
+    @Test
+    void shouldEmit_function_withValueReturnType_withParameters_usingGetText() {
+        // Arrange
+        BaseType baseTypeInt = BaseType.INT;
+        Type typeWithoutDimensions = new Type(baseTypeInt);
+        Function function = new Function(typeWithoutDimensions, "myFunction");
+        Variable variable1 = new Variable(typeWithoutDimensions, "myVariable1");
+        Variable variable2 = new Variable(typeWithoutDimensions, "myVariable2");
+
+        Parameter parameter1 = new Parameter(variable1);
+        Parameter parameter2 = new Parameter(variable2);
+
+        function.getParameters().add(parameter1);
+        function.getParameters().add(parameter2);
+
+        // Act
+        String emitValue = function.getText();
+
+        // Assert
+        assertEquals("define i32 @myFunction(i32 %myVariable1.param, i32 %myVariable2.param) {\n}", emitValue);
+    }
+
+    @Test
+    void shouldEmit_function_withReferenceReturnType_withoutParameters_usingGetText() {
+        // Arrange
+        BaseType baseTypeInt = BaseType.FLOAT;
+        Type typeWithOneDimension = new Type(baseTypeInt);
+        typeWithOneDimension.getDimensions().add(5);
+        Function function = new Function(typeWithOneDimension, "myFunction");
+
+        // Act
+        String emitValue = function.getText();
+
+        // Assert
+        assertEquals("define [5 x double]* @myFunction() {\n}", emitValue);
+    }
+
+    @Test
+    void shouldEmit_function_withReferenceReturnType_withParameters_usingGetText() {
+        // Arrange
+        BaseType baseTypeInt = BaseType.FLOAT;
+        Type typeWithOneDimension = new Type(baseTypeInt);
+        typeWithOneDimension.getDimensions().add(5);
+        Function function = new Function(typeWithOneDimension, "myFunction");
+
+        Variable variable1 = new Variable(typeWithOneDimension, "myVariable1");
+        Variable variable2 = new Variable(typeWithOneDimension, "myVariable2");
+
+        Parameter parameter1 = new Parameter(variable1);
+        Parameter parameter2 = new Parameter(variable2);
+
+        function.getParameters().add(parameter1);
+        function.getParameters().add(parameter2);
+
+        // Act
+        String emitValue = function.getText();
+
+        // Assert
+        assertEquals("define [5 x double]* @myFunction([5 x double]* %myVariable1.param, [5 x double]* %myVariable2.param) {\n}", emitValue);
+    }
+}
