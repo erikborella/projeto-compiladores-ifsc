1d7675fb7e959382032bb8ed37cb930b93c2b51a
erikborella <erikborella@gmail.com>
2024-09-21 03:29:45 -0300

    Adicionado configuração de CORS para a API

---
+++ projeto-compiladores-ifsc/src/main/java/ifsc/compiladores/projeto/API/configuration/CorsConfiguration.java
@@ -0,0 +1,56 @@
+package ifsc.compiladores.projeto.API.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ConfigurationProperties("cors")
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    private String[] allowedOrigins;
+    private String[] allowedMethods;
+    private String[] allowedHeaders;
+    private boolean allowCredentials;
+
+    public String[] getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    public String[] getAllowedMethods() {
+        return allowedMethods;
+    }
+
+    public void setAllowedMethods(String[] allowedMethods) {
+        this.allowedMethods = allowedMethods;
+    }
+
+    public String[] getAllowedHeaders() {
+        return allowedHeaders;
+    }
+
+    public void setAllowedHeaders(String[] allowedHeaders) {
+        this.allowedHeaders = allowedHeaders;
+    }
+
+    public boolean isAllowCredentials() {
+        return allowCredentials;
+    }
+
+    public void setAllowCredentials(boolean allowCredentials) {
+        this.allowCredentials = allowCredentials;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(allowedMethods)
+                .allowedHeaders(allowedHeaders)
+                .allowCredentials(allowCredentials);
+    }
+}
