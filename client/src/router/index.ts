4e8e07d86c4116cfa8cc9c4520ded793917cff02
erikborella <erikborella@gmail.com>
2024-09-26 00:06:43 -0300

    Modificado router para manter os estados da aplicação na query

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -5,10 +5,23 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router/auto';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+});
+
+function hasQueryParameters(route) {
+  return !!Object.keys(route.query).length;
+}
+
+router.beforeEach((to, from, next) => {
+  if (!hasQueryParameters(to) && hasQueryParameters(from)) {
+    const toWithQuery = Object.assign({}, to, { query: from.query });
+    next(toWithQuery);
+  } else {
+    next();
+  }
 })
 
 export default router
