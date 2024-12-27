0071b1bba23068f7e4dd142c4334708ba516410b
erikborella <erikborella@gmail.com>
2024-12-27 01:30:00 -0300

    Criado componente para a seleção de idiomas

--- client/components.d.ts
+++ client/components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    LanguageSelector: typeof import('./src/components/LanguageSelector.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     ScopeCard: typeof import('./src/components/ScopeCard.vue')['default']
---
+++ client/src/components/LanguageSelector.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-menu>
+
+    <template v-slot:activator="{ props }">
+      <v-btn icon="mdi-translate" v-bind="props"></v-btn>
+    </template>
+
+    <v-list>
+
+      <v-list-item
+        v-for="(lang, index) in languages"
+        :key="index"
+        :value="index"
+        @click="switchLanguage(lang.code)"
+      >
+        <v-list-item-title>{{ lang.name }}</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+
+  </v-menu>
+</template>
+
+<script lang="ts" setup>
+  import { useI18n } from 'vue-i18n';
+  import { languages } from '../locales';
+
+  const { locale } = useI18n();
+
+  function switchLanguage(lang: string) {
+     locale.value = lang;
+  }
+</script>
